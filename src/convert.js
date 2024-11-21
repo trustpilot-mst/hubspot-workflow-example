dd1939215810090c08a0e15131a465667377be37
Matthew Starkey <mats@trustpilot.com>
2024-11-21 12:54:55 +0000

    update to send portalId with request

--- src/convert.js
+++ src/convert.js
@@ -26,7 +26,7 @@ fs.readFile('src/index.js', 'utf8', (err, data) => {
       ],
       labels: {
         en: {
-          actionName: "Workflow Test 2.13 /workflows",
+          actionName: "Workflow Test 2.15 /workflows",
           actionCardContent: "Hello there my friend",
           inputFieldLabels: {
             "widgetName": "Widget Name"
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ exports.main = function(event, callback) {
 function transformRequest(event) { 
   return { 
     webhookUrl: event.webhookUrl, 
-    body: JSON.stringify(event.object), 
+    body: JSON.stringify({...event.object, portalId: event.origin.portalId}), 
     contentType: 'application/x-www-form-urlencoded', 
     accept: 'application/json', 
     httpMethod: 'POST' 
